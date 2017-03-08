0efd9e8746e2eeeadfcdb323495f60dbbe647792
Ant Chen <xiaoyong6906@126.com>
2017-03-08 20:48:47 +0800

    [JAVA][JDBC] Add first demo tutorial about JDBC

---
+++ jdbc/src/sql/company.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE COMPANY(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   AGE            INT     NOT NULL,
+   ADDRESS        CHAR(50),
+   SALARY         REAL
+); 
+
+INSERT INTO company(id, name, age, address, salary)
+    VALUES (0, 'Company 0', 0, 'Hua tuo Road', 110),
+    (1, 'Company 1', 1, 'Hua tuo Road', 111),
+    (2, 'Company 2', 2, 'Hua tuo Road', 112),
+    (3, 'Company 3', 3, 'Hua tuo Road', 113),
+    (4, 'Company 4', 4, 'Hua tuo Road', 114),
+    (5, 'Company 5', 5, 'Hua tuo Road', 115),
+    (6, 'Company 6', 6, 'Hua tuo Road', 116),
+    (7, 'Company 7', 7, 'Hua tuo Road', 117),
+    (8, 'Company 8', 8, 'Hua tuo Road', 118),
+    (9, 'Company 9', 9, 'Hua tuo Road', 119),
+    (10, 'Company 10', 10, 'Hua tuo Road', 120);
+    
--- jsp/src/main/java/com/jsp/util/DBHelper.java
+++ jsp/src/main/java/com/jsp/util/DBHelper.java
@@ -1,18 +1,11 @@
-package com.zhonghuasheng.jdbc;
+package com.jsp.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    private static final String driver = "org.postgresql.Driver";
+public class DBHelper {
+    private static final String driver = "";
     private static final String url = "jdbc:postgresql://localhost/jsp";
     private static final String user = "postgres";
     private static final String password = "postgres";
@@ -25,7 +18,6 @@ public class App
             e.printStackTrace();
         }
     }
-
     public static Connection getConnection() {
         if (conn == null) {
             try {
@@ -36,29 +28,5 @@ public class App
         }
 
         return conn;
-    }
-
-    public static void main( String[] args ) throws SQLException
-    {
-        Connection con = getConnection();
-        Statement statement = con.createStatement();
-        String sql = "SELECT * FROM company";
-        ResultSet rs = statement.executeQuery(sql);
-
-        while(rs.next()) {
-            System.out.println(rs.getInt("id"));
-        }
-
-        if (rs != null) {
-            rs.close();
-        }
-
-        if (statement != null) {
-            statement.close();
-        }
-
-        if (conn != null) {
-            conn.close();
-        }
     }
 }
