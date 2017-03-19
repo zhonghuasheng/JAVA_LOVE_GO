fdaacdb1a80f071ead1369369362910f88a6b104
Ant Chen <xiaoyong6906@126.com>
2017-03-19 13:18:11 +0800

    [JAVA][JDBC] Add executeUpdate demo and update README file

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
    * [JAVA JDBC 通过加载配置文件获取连接数据](./jdbc/src/main/java/com/zhonghuasheng/jdbc/learn03/GetProperties.java)
    * [JAVA JDBC Statement Update操作](./jdbc/src/main/java/com/zhonghuasheng/jdbc/learn02/StatementsDemo.java)
    * [JAVA JDBC ResultSet 获取查询结果]()
-   * [JAVA JDBC PreparedStatement Query语句中传入参数]()
+   * [JAVA JDBC PreparedStatement Query语句中传入参数](./jdbc/src/main/java/com/zhonghuasheng/jdbc/learn02/StatementsDemo.java)
 
 ### MyBatis
 
--- jdbc/src/main/java/com/zhonghuasheng/jdbc/learn02/StatementsDemo.java
+++ jdbc/src/main/java/com/zhonghuasheng/jdbc/learn02/StatementsDemo.java
@@ -5,32 +5,46 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class StatementsDemo {
 
-    private static Connection connection = null;
+    public static Connection getConnection() {
+        Connection connection = null;
 
-    static {
         try {
             Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost/jsp", "postgres", "postgres");
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static Connection getConnection() {
-        if (connection == null) {
-            try {
-                connection = DriverManager.getConnection("jdbc:postgresql://localhost/jsp", "postgres", "postgres");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
 
         return connection;
     }
 
-    public static void pPreparedStatement() {
+    public static void executeInsert() {
+        StringBuilder sb = new StringBuilder("INSERT INTO company(id, name, age, address, salary) VALUES (");
+        sb.append(12);
+        sb.append(", 'Company' ,");
+        sb.append(0);
+        sb.append(", 'Li Si Road' , ");
+        sb.append(110);
+        sb.append(");");
+
+        Connection connection = getConnection();
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sb.toString());
+            System.out.println("Succeed");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void executePreparedStatement() {
         String sql = "SELECT * FROM public.company WHERE id > ?";
         Connection connection = getConnection();
         try {
@@ -51,9 +65,7 @@ public class StatementsDemo {
     }
 
     public static void main(String[] args) {
-        pPreparedStatement();
-
+        executePreparedStatement();
+        executeInsert();
     }
-
-    
 }
