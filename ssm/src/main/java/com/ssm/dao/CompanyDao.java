bea9ad9f3cf256c42ab286acd3943093a7451f4f
Hua Sheng <xiaoyong6906@126.com>
2017-03-26 17:29:40 +0800

    [JAVA][Project-ssm] Add test method of getUserById and add spring test

--- ssm/src/main/java/com/ssm/dao/CompanyDao.java
+++
@@ -1,7 +0,0 @@
-package com.ssm.dao;
-
-import com.ssm.domain.Company;
-
-public interface CompanyDao {
-    Company getCompanyById(int id);
-}
--- ssm/src/main/java/com/ssm/service/CompanyService.java
+++
@@ -1,7 +0,0 @@
-package com.ssm.service;
-
-import com.ssm.domain.Company;
-
-public interface CompanyService {
-    Company getUserById(int id);
-}
