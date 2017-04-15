7b2dcfbf77a1259306602763cc38404bb9e16d50
Hua Sheng <xiaoyong6906@126.com>
2017-04-15 22:40:28 +0800

    [JAVA][Basic][Thread] Add demo about synchronized block and synchronized method

--- basic/src/main/java/com/zhonghuasheng/basic/thread/demo4/Count.java
+++ basic/src/main/java/com/zhonghuasheng/basic/thread/demo4/Count.java
@@ -3,6 +3,9 @@ package com.zhonghuasheng.basic.thread.demo4;
 public class Count {
 
     public void syncMethod() {
+        /**
+         * 同步代码块可以更精确的控制冲突限制访问区域
+         */
         synchronized(this) {
             try {
                 for (int i = 0; i < 5; i++) {
@@ -21,6 +24,18 @@ public class Count {
                 System.out.println(Thread.currentThread().getName() + " noSynchronized method " + i);
             }
         } catch(InterruptedException e) {
+        }
+    }
+
+    public synchronized void syncMethod2() {
+        synchronized(this) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    Thread.sleep(100);
+                    System.out.println(Thread.currentThread().getName() + " synchronized method " + i);
+                }
+            } catch(InterruptedException e) {
+            }
         }
     }
 }
--- basic/src/main/java/com/zhonghuasheng/basic/thread/demo4/SyncAndNoSyncMethodDemo.java
+++ basic/src/main/java/com/zhonghuasheng/basic/thread/demo4/SyncAndNoSyncMethodDemo.java
@@ -4,7 +4,10 @@ public class SyncAndNoSyncMethodDemo {
 
     public static void main(String[] args) {
         final Count count = new Count();
-
+        /**
+         * 当一个线程访问“某对象”的“synchronized方法”或者“synchronized代码块”时，其他线程
+         * 仍然可以访问“该对象”的非同步代码块
+         */
         new Thread(new Runnable() {
             public void run() {
                 count.syncMethod();
