84c60dc9bcaa1f47a970a9cce5bcd0de95b532f9
luke.chen <luke.chen@augmentum.com>
2019-05-17 17:28:33 +0800

    Add example about Hashtable and HashMap

--- basic/src/main/java/com/zhonghuasheng/basic/java/util/map/MapTest01.java
+++ basic/src/main/java/com/zhonghuasheng/basic/java/util/map/MapTest01.java
@@ -8,33 +8,35 @@ import java.util.Set;
 
 public class MapTest01 {
 
-	public static void main(String[] args) {
-		HashMap<String, String> hashMap = new HashMap<String, String>();
-		hashMap.put("1", "value1");
-		hashMap.put("2", "value2");
-		hashMap.put("3", "value3");
-		
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put("1", "value1");
+        hashMap.put("2", "value2");
+        hashMap.put("3", "value3");
+        
     // key视图
-		Set<String> keys = hashMap.keySet();
-		Iterator<String> iteratorKeys = keys.iterator();
-		while (iteratorKeys.hasNext()) {
-			System.out.println(iteratorKeys.next());
-		}
+        Set<String> keys = hashMap.keySet();
+        Iterator<String> iteratorKeys = keys.iterator();
+        while (iteratorKeys.hasNext()) {
+            System.out.println(iteratorKeys.next());
+        }
 
     // value视图
-		Collection<String> values = hashMap.values();
-		Iterator<String> iteratorValues = values.iterator();
-		while (iteratorValues.hasNext()) {
-			System.out.println(iteratorValues.next());
-		}
+        Collection<String> values = hashMap.values();
+        Iterator<String> iteratorValues = values.iterator();
+        while (iteratorValues.hasNext()) {
+            System.out.println(iteratorValues.next());
+        }
 
     // key-value视图
-		Set<Entry<String, String>> entrySets = hashMap.entrySet();
-		Iterator<Entry<String, String>> iteratorEntrySets = entrySets.iterator();
-		while (iteratorEntrySets.hasNext()) {
-			Entry<String, String> entry = iteratorEntrySets.next();
-			System.out.println(String.format("key: %s, value: %s", entry.getKey(), entry.getValue()));
-		}
-	}
+        Set<Entry<String, String>> entrySets = hashMap.entrySet();
+        Iterator<Entry<String, String>> iteratorEntrySets = entrySets.iterator();
+        while (iteratorEntrySets.hasNext()) {
+            Entry<String, String> entry = iteratorEntrySets.next();
+            System.out.println(String.format("key: %s, value: %s", entry.getKey(), entry.getValue()));
+        }
+
+        hashMap.put(null, null);
+    }
 
 }
