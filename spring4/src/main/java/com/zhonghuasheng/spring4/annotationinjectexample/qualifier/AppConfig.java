3556cf64469469eabe4c22e207ea6748b2aaba97
luke.chen <luke.chen@augmentum.com>
2019-06-06 14:35:07 +0800

    Add example about using @Qualifier annotation in spring4

---
+++ spring4/src/main/java/com/zhonghuasheng/spring4/annotationinjectexample/qualifier/AppConfig.java
@@ -0,0 +1,9 @@
+package com.zhonghuasheng.spring4.annotationinjectexample.qualifier;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.zhonghuasheng.spring4.annotationinjectexample.qualifier")
+public class AppConfig {
+}
--- spring4/src/main/java/com/zhonghuasheng/spring4/annotationinjectexample/qualifier/AppMain.java
+++ spring4/src/main/java/com/zhonghuasheng/spring4/annotationinjectexample/qualifier/AppMain.java
@@ -1,4 +1,4 @@
-package com.zhonghuasheng.spring4.annotationinjectexample.autowired;
+package com.zhonghuasheng.spring4.annotationinjectexample.qualifier;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -7,7 +7,7 @@ public class AppMain {
 
     public static void main(String[] args) {
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        Driver driver = (Driver) context.getBean("driver");
-        System.out.println(driver);
+        Bond bond = (Bond) context.getBean("bond");
+        bond.showCar();
     }
 }
