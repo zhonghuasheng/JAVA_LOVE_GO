8f9e9b6ed082089bdb0e8330ffe0416ac3a30ca7
Luke Chen <luke.chen@augmentum.com>
2019-09-04 14:11:36 +0800

    feat(rabbitmq): Add a basic practice for rabbitmq in Java platform

---
+++ rabbitmq/src/main/java/com/zhonghuasheng/rabbitmq/Producer.java
@@ -0,0 +1,27 @@
+package com.zhonghuasheng.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    private static final String QUEUE_NAME = "test.simple.queue";
+
+    public void sendMessage() {
+        try {
+            Connection connection = ConnectionUtils.getConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String msg = "Hello World! 你好世界！";
+            channel.basicPublish("", QUEUE_NAME, null, msg.getBytes());
+            channel.close();
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ rabbitmq/src/main/java/com/zhonghuasheng/rabbitmq/basic/App.java
@@ -0,0 +1,20 @@
+package com.zhonghuasheng.rabbitmq.basic;
+
+import com.zhonghuasheng.rabbitmq.Consumer;
+import com.zhonghuasheng.rabbitmq.Producer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException {
+        for (int i = 0; i < 9; i++) {
+            new Producer().sendMessage();
+            System.out.println(i);
+        }
+        Thread.sleep(10000);
+        new Consumer().receiveMessage();
+    }
+}
