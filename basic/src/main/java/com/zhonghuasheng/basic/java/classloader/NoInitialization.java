f27f3ce6c37b39075a64471e484ffa00a54045b1
Hua Sheng <xiaoyong6906@126.com>
2020-02-08 16:57:56 +0800

    feat(classloader const): 编译阶段静态常量存储到了常量池，对其引用不会触发其类的初始化

--- basic/src/main/java/com/zhonghuasheng/basic/java/classloader/NoInitialization.java
+++ basic/src/main/java/com/zhonghuasheng/basic/java/classloader/NoInitialization.java
@@ -3,12 +3,15 @@ package com.zhonghuasheng.basic.java.classloader;
 public class NoInitialization {
 
     public static void main(String[] args) {
-        System.out.println(SubClass.value);
         /**
-         * 程序执行结果。
-         * SuperClass init!
-         * 123
-         * 子类的静态代码块没有执行，说明子类没有初始化。静态字段，只有直接定义这个字段的类才会被初始化。
-         */
+        * 程序执行结果。
+        * SuperClass init!
+        * 123
+        * 子类的静态代码块没有执行，说明子类没有初始化。静态字段，只有直接定义这个字段的类才会被初始化。
+        */
+        System.out.println(SubClass.value);
+
+        // 编译阶段静态常量存储到了常量池
+        System.out.println(ConstClass.HELLOWORLD);
     }
 }
