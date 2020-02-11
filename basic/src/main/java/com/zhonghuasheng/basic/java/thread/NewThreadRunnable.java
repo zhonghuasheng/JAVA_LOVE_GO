b781c1e3269ac59ab21ccee69d4bd897237f4af5
Hua Sheng <xiaoyong6906@126.com>
2020-02-11 15:38:37 +0800

    feat(thread): 添加了通过实现Runnable接口来创建新的线程

--- basic/src/main/java/com/zhonghuasheng/basic/java/thread/NewThreadRunnable.java
+++ basic/src/main/java/com/zhonghuasheng/basic/java/thread/NewThreadRunnable.java
@@ -3,7 +3,13 @@ package com.zhonghuasheng.basic.java.thread;
 public class NewThreadRunnable {
 
     public static void main(String[] args) {
+        // run方法运行
         new NewRunnable().run();
+
+        // start方法运行
+        NewRunnable newRunnable = new NewRunnable();
+        Thread thread = new Thread(newRunnable);
+        thread.start();
     }
 }
 
