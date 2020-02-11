8b07bca71adfdcf3d095e81ac22d1574b9fead85
Hua Sheng <xiaoyong6906@126.com>
2020-02-11 16:02:45 +0800

    feat(thread): 添加了通过实现Callable接口(FutureTask)来实现创建新的线程

---
+++ basic/src/main/java/com/zhonghuasheng/basic/java/thread/NewThreadCallable.java
@@ -0,0 +1,39 @@
+package com.zhonghuasheng.basic.java.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class NewThreadCallable {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Callable newCallable = new NewCallable();
+        FutureTask futureTask = new FutureTask(newCallable);
+        Thread thread = new Thread(futureTask);
+        thread.start();
+        Object result = futureTask.get();
+        System.out.println(String.valueOf(result));
+
+        Callable<Integer> newCallable2 = new NewCallable2();
+        FutureTask<Integer> task = new FutureTask(newCallable2);
+        new Thread(task).start();
+        Integer i = task.get();
+        System.out.println(i);
+    }
+}
+
+class NewCallable implements Callable {
+
+    @Override
+    public Object call() throws Exception {
+        return "Hello World";
+    }
+}
+
+class NewCallable2 implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        return 1;
+    }
+}
