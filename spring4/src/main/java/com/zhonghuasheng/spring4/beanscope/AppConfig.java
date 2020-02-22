f67ec0ba99713ff6548653293c903f4beb774aa1
Hua Sheng <xiaoyong6906@126.com>
2020-02-22 10:35:03 +0800

    feat(bean scope): Spring Bean默认的bean scope是singleton,可以通过配置@Scope来开启

---
+++ spring4/src/main/java/com/zhonghuasheng/spring4/beanscope/AppConfig.java
@@ -0,0 +1,9 @@
+package com.zhonghuasheng.spring4.beanscope;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.zhonghuasheng.spring4.beanscope")
+public class AppConfig {
+}
---
+++ spring4/src/main/java/com/zhonghuasheng/spring4/beanscope/UserPrototypeService.java
@@ -0,0 +1,10 @@
+package com.zhonghuasheng.spring4.beanscope;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class UserPrototypeService {
+}
