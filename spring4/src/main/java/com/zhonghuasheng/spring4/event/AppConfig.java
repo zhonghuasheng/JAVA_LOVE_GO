d55d6eaa02a646d28200135dd486e9817794272e
Hua Sheng <xiaoyong6906@126.com>
2020-02-23 15:24:20 +0800

    feat(spring event): 在Spring中通过继承ApplicationEvent来定义事件，通过实现ApplicationListener来监听，通过ApplicationContext.publishEvent来发布事件

---
+++ spring4/src/main/java/com/zhonghuasheng/spring4/event/AppConfig.java
@@ -0,0 +1,9 @@
+package com.zhonghuasheng.spring4.event;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.zhonghuasheng.spring4.event")
+public class AppConfig {
+}
