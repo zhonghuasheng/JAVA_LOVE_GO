308d52c760c1253ee5968b527f148676140dd9ce
Hua Sheng <xiaoyong6906@126.com>
2020-02-28 11:58:43 +0800

    feat(springmvc): 添加了静态资源映射

--- springmvc/nowebxml/src/main/java/com/zhonghuasheng/springmvc/config/MvcConfig.java
+++ springmvc/nowebxml/src/main/java/com/zhonghuasheng/springmvc/config/MvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -22,5 +23,11 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         viewResolver.setViewClass(JstlView.class); // 指定解析视图的类
 
         return viewResolver;
+    }
+
+    // 静态资源映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
     }
 }
