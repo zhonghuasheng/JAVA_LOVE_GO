909eb8c0d77ac8257002394f269cc89147b588cb
Hua Sheng <xiaoyong6906@126.com>
2020-02-28 17:29:06 +0800

    feat(springmvc): 通过@ModelAttribut注解在方法上来实现数据的全局绑定，前提是类被@ControllerAdvice注解

--- springmvc/nowebxml/src/main/java/com/zhonghuasheng/springmvc/advice/ExceptionHandlerAdvice.java
+++ springmvc/nowebxml/src/main/java/com/zhonghuasheng/springmvc/advice/ExceptionHandlerAdvice.java
@@ -1,13 +1,19 @@
 package com.zhonghuasheng.springmvc.advice;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
 
+    // 全局异常处理
     @ExceptionHandler(value = Exception.class)
     public ModelAndView exceptionHandler(Exception exception, WebRequest request) {
         ModelAndView mv = new ModelAndView("error");
@@ -22,5 +28,29 @@ public class ExceptionHandlerAdvice {
         mv.addObject("errorMessage", exception.getMessage());
 
         return mv;
+    }
+
+    // 全局数据绑定
+    @ModelAttribute(name = "globalData")
+    public Map<String, Object> data() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "Luke");
+        map.put("age", "18");
+
+        return map;
+    }
+
+    @ModelAttribute(name = "globalData2")
+    public Map<String, Object> aData() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "Luke");
+        map.put("age", "18");
+
+        return map;
+    }
+
+    @ModelAttribute
+    public void addModelAttribute(Model model) {
+        model.addAttribute("msg", "Msg from ModelAttribute");
     }
 }
--- springmvc/nowebxml/src/main/java/com/zhonghuasheng/springmvc/controller/AdviceController.java
+++ springmvc/nowebxml/src/main/java/com/zhonghuasheng/springmvc/controller/AdviceController.java
@@ -1,7 +1,13 @@
 package com.zhonghuasheng.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/advice")
@@ -11,5 +17,22 @@ public class AdviceController {
     @RequestMapping(value = "/error")
     public String error() {
         throw new IllegalArgumentException("ControllerAdvice exception example");
+    }
+
+    // 获取全局数据
+    @GetMapping("/data")
+    @ResponseBody
+    public String data(Model model) {
+        Map<String, Object> map = model.asMap();
+        System.out.println(map); // 打印所有全局数据
+        System.out.println(map.get("globalData2")); // 打印指定数据
+
+        return "Global data";
+    }
+
+    @GetMapping("/data2")
+    @ResponseBody
+    public String data2(@ModelAttribute("msg") String msg) {
+        return "Global data 2: " + msg;
     }
 }
