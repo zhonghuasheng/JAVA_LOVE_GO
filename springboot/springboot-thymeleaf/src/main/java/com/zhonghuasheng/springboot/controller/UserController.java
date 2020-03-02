62cd69c45fe589a8933d37f84fb6344ee739473d
Hua Sheng <xiaoyong6906@126.com>
2020-03-02 17:24:15 +0800

    feat(thymeleaf): 添加了list/view/edit这几个action和view，同时演示了href动态使用的例子

--- springboot/springboot-thymeleaf/src/main/java/com/zhonghuasheng/springboot/controller/UserController.java
+++ springboot/springboot-thymeleaf/src/main/java/com/zhonghuasheng/springboot/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.zhonghuasheng.springboot.controller;
 
+import com.zhonghuasheng.springboot.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -12,5 +17,31 @@ public class UserController {
         model.addAttribute("info", "user/list");
 
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String view(Model model) {
+        User user = new User("0001", "Tom", "123456");
+        model.addAttribute("user", user);
+
+        return "user/view";
+    }
+
+    @GetMapping("/user/list")
+    public String list(Model model) {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("0001", "Tom", "123456"));
+        userList.add(new User("0002", "Jom", "123457"));
+        userList.add(new User("0003", "Kom", "123458"));
+        model.addAttribute("userList", userList);
+
+        return "user/list";
+    }
+
+    @GetMapping("/user/edit/{userId}")
+    public String edit(@PathVariable String userId, Model model) {
+        model.addAttribute("user", new User(userId, "Tom", "123"));
+
+        return "user/view";
     }
 }
