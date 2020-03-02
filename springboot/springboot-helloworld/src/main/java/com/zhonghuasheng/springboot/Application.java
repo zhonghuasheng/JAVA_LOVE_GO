9bdab66a76ecf787449d4222e52f34e87998d7ee
Hua Sheng <xiaoyong6906@126.com>
2020-03-02 11:33:34 +0800

    feat(springboot): 添加了最基本的SpringBoot项目，同时配置了禁用CLRF

---
+++ springboot/springboot-helloworld/src/main/java/com/zhonghuasheng/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.zhonghuasheng.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.run();
+    }
+}
