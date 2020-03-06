5013f5cf5fc0c57df738cc0f93c371257ff7d315
Hua Sheng <xiaoyong6906@126.com>
2020-03-06 20:31:32 +0800

    feat(springboot interceptor): 添加自定义拦截器

---
+++ springboot/springboot-web/src/main/java/com/zhonghuasheng/springboot/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.zhonghuasheng.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, SpringBoot Web";
+    }
+}
