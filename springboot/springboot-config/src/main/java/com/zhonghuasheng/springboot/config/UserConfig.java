a874c7d25cac9c2f0c6e363d6c4708faeffbb367
Hua Sheng <xiaoyong6906@126.com>
2020-03-06 15:01:42 +0800

    feat(springboot properties): 给对象属性(日期、集合)注入属性中的值

--- springboot/springboot-config/src/main/java/com/zhonghuasheng/springboot/config/UserConfig.java
+++ springboot/springboot-config/src/main/java/com/zhonghuasheng/springboot/config/UserConfig.java
@@ -1,7 +1,6 @@
 package com.zhonghuasheng.springboot.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
