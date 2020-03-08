abab8454fb592740633b2cd34268ea147f4264a8
Hua Sheng <xiaoyong6906@126.com>
2020-03-08 18:34:50 +0800

    feat(seckill): 添加了判断是否存在，递增，递减的RedisService接口

--- seckill/src/main/java/com/zhonghuasheng/seckill/redis/RedisService.java
+++ seckill/src/main/java/com/zhonghuasheng/seckill/redis/RedisService.java
@@ -14,6 +14,9 @@ public class RedisService {
     @Autowired
     JedisPool jedisPool;
 
+    /*
+     * 获取对象
+     */
     public <T> T get(KeyPrefix prefix, String key, Class<T> clazz) {
         Jedis jedis = null;
         try {
@@ -26,6 +29,9 @@ public class RedisService {
         }
     }
 
+    /*
+     * 设置对象
+     */
     public <T> boolean set(KeyPrefix prefix, String key, T value) {
         Jedis jedis = null;
         try {
@@ -34,8 +40,65 @@ public class RedisService {
             if (str == null || str.length() <= 0) {
                 return false;
             }
-            jedis.set(prefix.getPrefix() + key, str);
+            int seconds = prefix.expireSeconds();
+            String realKey = prefix.getPrefix() + key;
+            if (seconds <= 0) {
+                jedis.set(realKey, str);
+            } else {
+                jedis.setex(realKey, seconds, str);
+            }
+
             return true;
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    /*
+     * 判断key是否存在
+     */
+    public <T> boolean exists(KeyPrefix prefix, String key, T value) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String realKey = prefix.getPrefix() + key;
+
+            return jedis.exists(realKey);
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    /*
+     * 增加值
+     * Redis Incr 命令将 key 中储存的数字值增一。
+     * 如果 key 不存在，那么 key 的值会先被初始化为 0 ，然后再执行 INCR 操作
+     */
+    public <T> Long incr(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String realKey = prefix.getPrefix() + key;
+
+            return jedis.incr(realKey);
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    /*
+     * 减少值
+     * Redis Decr 命令将 key 中储存的数字值减一。
+     * 如果 key 不存在，那么 key 的值会先被初始化为 0 ，然后再执行 DECR 操作
+     * 一直使用decr会出现负值
+     */
+    public <T> Long decr(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String realKey = prefix.getPrefix() + key;
+
+            return jedis.decr(realKey);
         } finally {
             returnToPool(jedis);
         }
