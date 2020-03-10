586ecc42936bda5c354e59376848ce29d8494761
Hua Sheng <xiaoyong6906@126.com>
2020-03-10 14:57:44 +0800

    feat(seckill table): 添加了秒杀订单表

--- seckill/src/main/docs/20200309-0-create-goods-table.sql
+++ seckill/src/main/docs/20200309-0-create-goods-table.sql
@@ -28,3 +28,26 @@ CREATE TABLE seckill_goods(
 INSERT INTO seckill_goods VALUES
 (1, 1, 0.01, 4, '2020-03-10 12:00:00', '2020-03-12 12:00:00'),
 (2, 2, 0.01, 9, '2020-03-10 11:00:00', '2020-03-12 12:00:00')
+
+CREATE TABLE order_info(
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  user_id BIGINT(20) DEFAULT NULL COMMENT '用户ID',
+  goods_id BIGINT(20) DEFAULT NULL COMMENT '商品ID',
+  delivery_addr_id BIGINT(20) DEFAULT NULL COMMENT '收货地址ID',
+  goods_name VARCHAR(16) DEFAULT NULL COMMENT '冗余过来的商品名',
+  goods_count INT(11) DEFAULT '0' COMMENT '商品数量',
+  goods_price DECIMAL(10, 2) DEFAULT '0.00' COMMENT '商品单价',
+  order_channel TINYINT(4) DEFAULT '0' COMMENT '1pc, 2android, 3ios',
+  status TINYINT(4) DEFAULT '0' COMMENT '订单状态，0新建未支付，1已支付，2已发货，3已收货，4已退款，5已完成',
+  create_date DATETIME DEFAULT NULL COMMENT '订单创建时间',
+  pay_date DATETIME DEFAULT NULL COMMENT '支付时间',
+  PRIMARY KEY(id)
+) DEFAULT CHARSET=UTF8;
+
+CREATE TABLE seckill_order(
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  user_id BIGINT(20) DEFAULT NULL COMMENT '用户ID',
+  order_id BIGINT(20) DEFAULT NULL COMMENT '订单ID',
+  goods_id BIGINT(20) DEFAULT NULL COMMENT '商品ID',
+  PRIMARY KEY(id)
+) DEFAULT CHARSET=UTF8;
