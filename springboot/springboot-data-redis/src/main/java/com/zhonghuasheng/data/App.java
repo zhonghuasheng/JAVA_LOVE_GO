8072fdbe6ab81570bfe1be156002ffab19bc4d56
Hua Sheng <xiaoyong6906@126.com>
2020-03-31 12:44:26 +0800

    feat(lettuce): SpringBoot集成Lettuce实现对Redis Cluster的使用

---
+++ springboot/springboot-data-redis/src/main/java/com/zhonghuasheng/data/App.java
@@ -0,0 +1,12 @@
+package com.zhonghuasheng.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ springboot/springboot-data-redis/src/main/java/com/zhonghuasheng/data/config/RedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.zhonghuasheng.data.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Resource
+    private LettuceConnectionFactory lettuceConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate() {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(lettuceConnectionFactory);
+
+        return template;
+    }
+}
