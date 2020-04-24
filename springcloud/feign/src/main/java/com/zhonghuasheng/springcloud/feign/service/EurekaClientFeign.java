e538d2b9debbbf0382554245b5f89854164e4fab
Hua Sheng <xiaoyong6906@126.com>
2020-04-24 21:14:45 +0800

    feat(hystrix): 在feigin中使用hystrix

--- springcloud/feign/src/main/java/com/zhonghuasheng/springcloud/feign/service/EurekaClientFeign.java
+++ springcloud/feign/src/main/java/com/zhonghuasheng/springcloud/feign/service/EurekaClientFeign.java
@@ -1,11 +1,12 @@
 package com.zhonghuasheng.springcloud.feign.service;
 
 import com.zhonghuasheng.springcloud.feign.config.FeignConfig;
+import com.zhonghuasheng.springcloud.feign.config.HiHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "eureka-client", configuration = FeignConfig.class)
+@FeignClient(value = "eureka-client", configuration = FeignConfig.class, fallback = HiHystrix.class)
 public interface EurekaClientFeign {
 
     @GetMapping("/hi")
