1482a985182ff7b9ae7a55fdd45457fe3c2dfc58
Hua Sheng <xiaoyong6906@126.com>
2020-05-31 20:05:14 +0800

    feat(redis incr): 开发了简易版本的转发量统计

---
+++ springboot/springboot-redistemplate/src/main/java/com/zhonghuasheng/springboot/service/impl/ForwardServiceImpl.java
@@ -0,0 +1,32 @@
+package com.zhonghuasheng.springboot.service.impl;
+
+import com.zhonghuasheng.springboot.service.ForwardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class ForwardServiceImpl implements ForwardService {
+
+    private static final String PREFIX="music:forward";
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void forward(Long id) {
+        if (stringRedisTemplate.hasKey(PREFIX)) {
+            stringRedisTemplate.opsForValue().increment(PREFIX, 1);
+            System.out.println("After incr, the value is: " + stringRedisTemplate.opsForValue().get(PREFIX));
+        } else {
+            setKey(PREFIX, "101");
+        }
+    }
+
+    public synchronized void setKey(String key, String value) {
+        stringRedisTemplate.opsForValue().setIfAbsent(key, value, 24, TimeUnit.HOURS);
+        System.out.println("After execute setKey method, the value is: " + stringRedisTemplate.opsForValue().get(key));
+    }
+}
