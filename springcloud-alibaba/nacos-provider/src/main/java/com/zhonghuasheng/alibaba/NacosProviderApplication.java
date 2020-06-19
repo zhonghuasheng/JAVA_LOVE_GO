eafb40f2dd70a72e9a647f5e023e122bc7d21bc0
Hua Sheng <xiaoyong6906@126.com>
2020-06-19 22:00:00 +0800

    feat(nacos): 初始化springcloud nacos服务

---
+++ springcloud-alibaba/nacos-provider/src/main/java/com/zhonghuasheng/alibaba/NacosProviderApplication.java
@@ -0,0 +1,14 @@
+package com.zhonghuasheng.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+}
