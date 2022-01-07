f672079030a20cc5c0ee2afc90162ac78dd1dad6
chenyong <>
2022-01-07 18:52:03 +0800

    feat(sph): 开发demo

--- springcloud/hystrix/src/main/java/com/zhonghuasheng/hystrix/HiController.java
+++ springcloud/hystrix/src/main/java/com/zhonghuasheng/hystrix/HiController.java
@@ -10,13 +10,24 @@ public class HiController {
 
     @GetMapping("/hystrix")
     @HystrixCommand(fallbackMethod = "fallbackMethod", commandProperties = {
+        // 设置超时时间，默认1秒
         @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000"),
+        // 监控时间，默认5秒
         @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000"),
+        // 失败次数，默认20次
         @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "20"),
+        // 失败了，默认50%
         @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50")
     })
     public String sayHi() {
-        int i = 3 / 0;
+/*      // 1. 容错，此处逻辑抛出异常，会走熔断方法
+        int i = 3 / 0;*/
+        // 2. 响应超时熔断
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+
+        }
         return "hi";
     }
 
