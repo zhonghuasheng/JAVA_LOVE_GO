37c7dd4a66543394284bd1a2e7d40c52df620904
chenyong <>
2022-01-17 20:11:11 +0800

    feat(01): 优化查找两数之和算法

--- thinking/src/main/java/com/zhonghuasheng/leetcode/L01Sum.java
+++ thinking/src/main/java/com/zhonghuasheng/leetcode/L01Sum.java
@@ -1,6 +1,8 @@
 package com.zhonghuasheng.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -23,8 +25,16 @@ public class L01Sum {
         int[] array = {2, 7, 11, 15};
         int[] result = new L01Sum().twoSum(array, 9);
         System.out.println(Arrays.toString(result));
+        int[] result2 = new L01Sum().towSum(array, 9);
+        System.out.println(Arrays.toString(result2));
     }
 
+    /**
+     * 时间复杂度O(n2)
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
@@ -37,5 +47,23 @@ public class L01Sum {
             }
         }
         return result;
+    }
+
+    /**
+     * 时间复杂度 O(N)，N为数组中的元素数量
+     * 空间复杂度O(N), N为数组中的元素数量
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] towSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[] {hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
