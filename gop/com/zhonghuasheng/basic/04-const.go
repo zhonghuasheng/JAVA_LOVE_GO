16d56ca7e9e29bb172292ed57d37d3e723dc6e7d
Luke Chen <xiaoyong6906@126.com>
2022-02-13 15:08:43 +0800

    feat(go): 添加iota示例

---
+++ gop/com/zhonghuasheng/basic/04-const.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+/**
+常量
+const可以定义枚举类型
+*/
+const (
+	BEIJING   = 0
+	SHANGHAI  = 1
+	SHENZHENG = 2
+)
+
+// go中提供了一个关键字iota，每行的iota都会累加1  注意是行
+const (
+	A = iota // iota=0
+	B        // iota=1
+	C        // iota=2
+)
+
+const (
+	A2 = 10 * iota // iota=0
+	B2             // iota=1
+	C2             // iota=2
+)
+
+const (
+	a, b = iota + 1, iota + 2 // iota=0, a=1, b = 2
+	c, d                      // iota=1, c=iota+1, d=iota+2
+	e, f                      // iota=2, e=iota+1, d=iota+2
+	g, h = iota * 2, iota * 3 // iota=3, g=6,h=9
+	i, k                      // iota=4, i=iota*2 8 k=iota*3 12
+)
+
+func main() {
+	// 常量（只读属性）
+	const length int = 20
+	fmt.Println(length)
+	fmt.Println(BEIJING)
+	fmt.Println(C)  // 2
+	fmt.Println(C2) // 20
+	fmt.Println(k)
+}
--- thinking/src/main/java/com/zhonghuasheng/algorithm/lru/LRUMain.java
+++ thinking/src/main/java/com/zhonghuasheng/algorithm/lru/LRUMain.java
@@ -6,6 +6,19 @@ package com.zhonghuasheng.algorithm.lru;
 public class LRUMain {
 
     public static void main(String[] args) {
+        // 初始化容量为3的LRU
+        LRUCache lruCache = new LRUCache(3);
+        lruCache.set(1, 1);
+        lruCache.set(2, 2);
+        lruCache.set(3, 3);
+        // 设置4，4，结果为2，3，4
+        lruCache.set(4, 4);
+        // get 2，结果为3，4，2
+        Node node = lruCache.get(2);
+        System.out.println(node.value);
+        Node node2 = lruCache.get(2);
+        System.out.println(node2.value);
+
         String str = "1";
         Integer result = getFirstNum(str);
         System.out.println(result);
