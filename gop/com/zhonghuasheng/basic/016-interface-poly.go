ce490a8ac002886f07899d2a8c62f3ab9221adb7
Luke Chen <xiaoyong6906@126.com>
2022-02-22 23:04:30 +0800

    feat(go): 添加go语言多态示例

---
+++ gop/com/zhonghuasheng/basic/016-interface-poly.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// 定义一个接口，本质是一个指针
+type Animal interface {
+	Sleep()
+	GetColor() string
+}
+
+// 定义一个类，实现接口的方法
+type Dog struct {
+	color string
+}
+
+func (this *Dog) Sleep() {
+	fmt.Println("Dog sleep")
+}
+
+func (this *Dog) GetColor() string {
+	return this.color
+}
+
+type Cat struct {
+	color string
+}
+
+func (this *Cat) Sleep() {
+	fmt.Println("Cat sleep")
+}
+
+func (this *Cat) GetColor() string {
+	return this.color
+}
+
+func main() {
+	// 第一种调用方式：cat和dog都实现了Animal，对其中的方法也都实现了，多态的本质
+	var animal Animal
+	animal = &Dog{color: "Yello"}
+	animal.Sleep()
+	fmt.Println(animal.GetColor())
+	animal = &Cat{color: "White"}
+	animal.Sleep()
+	fmt.Println(animal.GetColor())
+	fmt.Println("---------------")
+	// 第二种调用方式
+	Show(&Dog{color: "color1"})
+	Show(&Cat{color: "color2"})
+}
+
+func Show(animal Animal) {
+	animal.Sleep() // 多态
+	fmt.Println(animal.GetColor())
+}
