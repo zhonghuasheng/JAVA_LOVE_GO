d8545bfdea5a372bd9456fd6d1b55adc4a73d431
Luke Chen <xiaoyong6906@126.com>
2022-03-01 22:39:22 +0800

    feat(go): 优化结构代码

--- gop/com/zhonghuasheng/basic/015-class.go
+++ gop/com/zhonghuasheng/basic/015-class.go
@@ -24,8 +24,10 @@ func main() {
 	fmt.Println(p) // 打印出来的还是Luke Chen
 	p.setName2("ttttt")
 	fmt.Println(p)
+	fmt.Println("p=", p, " &p=", &p) // p= {ttttt 0}  &p= &{ttttt 0}
 }
 
+// 我觉得结构体和结构体指针 可以认为是一个意思，需要区别的是传指针可以修改结构体的字段的值
 func (people *Human) getName2() string {
 	return people.Name
 }
