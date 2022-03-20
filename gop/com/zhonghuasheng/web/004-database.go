0a9095688e41d88fb09f12aac3f4727dd78a86c7
Luke Chen <xiaoyong6906@126.com>
2022-03-20 17:12:18 +0800

    feat(go): 添加查询示例

--- gop/com/zhonghuasheng/web/004-database.go
+++ gop/com/zhonghuasheng/web/004-database.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "xxx:xxxx@tcp(49.235.118.8:3306)/guestdb?charset=utf8")
+	db, err := sql.Open("mysql", "xxx:xxx@tcp(49.235.118.8:3306)/guestdb?charset=utf8&parseTime=true")
 
 	if err != nil {
 		fmt.Println(err)
@@ -28,14 +28,28 @@ func main() {
 	// 	fmt.Println(createErr)
 	// }
 
-	// 插入数据
-	username := "luke"
-	password := "123"
-	createdAt := time.Now()
-	result, err := db.Exec("INSERT INTO users (username, password, created_at) VALUES(?,?,?)", username, password, createdAt)
-	userID, insertErr := result.LastInsertId()
-	fmt.Println("LastInsertId =", userID)
-	if insertErr != nil {
-		fmt.Println(insertErr)
+	// // 插入数据
+	// username := "luke"
+	// password := "123"
+	// createdAt := time.Now()
+	// result, err := db.Exec("INSERT INTO users (username, password, created_at) VALUES(?,?,?)", username, password, createdAt)
+	// userID, insertErr := result.LastInsertId()
+	// fmt.Println("LastInsertId =", userID)
+	// if insertErr != nil {
+	// 	fmt.Println(insertErr)
+	// }
+
+	// 查询数据
+	var (
+		id        int
+		username  string
+		password  string
+		createdAt time.Time
+	)
+	selectQuery := "SELECT id, username, password, created_at FROM users WHERE id = ?"
+	selectErr := db.QueryRow(selectQuery, 1).Scan(&id, &username, &password, &createdAt)
+	if selectErr != nil {
+		fmt.Println(selectErr)
 	}
+	fmt.Printf(" id=%d \n username=%s \n password=%s \n createdAt=%s", id, username, password, createdAt)
 }
