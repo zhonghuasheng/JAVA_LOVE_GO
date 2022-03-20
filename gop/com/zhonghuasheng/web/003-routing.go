e6d69aa5d81149f2409815cca1b733f2e0714686
Luke Chen <xiaoyong6906@126.com>
2022-03-20 12:35:14 +0800

    feat(go): 添加mysql示例

--- gop/com/zhonghuasheng/web/003-routing.go
+++ gop/com/zhonghuasheng/web/003-routing.go
@@ -17,6 +17,14 @@ func main() {
 		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
 
+	// 扩展。将请求与方法绑定
+	r.HandleFunc("/band", ReadBook).Methods("GET")
+
 	http.ListenAndServe(":8080", r)
 	// http://localhost:8080/books/%E5%B9%B3%E5%87%A1%E7%9A%84%E4%B8%96%E7%95%8C/page/101
+}
+
+func ReadBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Read book here")
+	// http://localhost:8080/band 按照这种特性其实可以扩展一个api method的自动映射
 }
