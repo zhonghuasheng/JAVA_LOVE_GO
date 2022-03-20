1d975f05ae06460a7eb7afae6638f4664566e638
Luke Chen <xiaoyong6906@126.com>
2022-03-20 16:35:29 +0800

    feat(go): 添加插入用户示例

--- gop/com/zhonghuasheng/web/004-database.go
+++ gop/com/zhonghuasheng/web/004-database.go
@@ -3,27 +3,39 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "user:password@tcp(49.235.118.8:3306)/guestdb?charset=utf8")
+	db, err := sql.Open("mysql", "xxx:xxxx@tcp(49.235.118.8:3306)/guestdb?charset=utf8")
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// 创建表
-	createSQL := `CREATE TABLE users (
-        id INT AUTO_INCREMENT,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL,
-        created_at DATETIME,
-        PRIMARY KEY (id)
-    );`
-	_, createErr := db.Exec(createSQL)
-	if createErr != nil {
-		fmt.Println(createErr)
+	// // 创建表
+	// createSQL := `CREATE TABLE users (
+	//     id INT AUTO_INCREMENT,
+	//     username TEXT NOT NULL,
+	//     password TEXT NOT NULL,
+	//     created_at DATETIME,
+	//     PRIMARY KEY (id)
+	// );`
+	// _, createErr := db.Exec(createSQL)
+	// if createErr != nil {
+	// 	fmt.Println(createErr)
+	// }
+
+	// 插入数据
+	username := "luke"
+	password := "123"
+	createdAt := time.Now()
+	result, err := db.Exec("INSERT INTO users (username, password, created_at) VALUES(?,?,?)", username, password, createdAt)
+	userID, insertErr := result.LastInsertId()
+	fmt.Println("LastInsertId =", userID)
+	if insertErr != nil {
+		fmt.Println(insertErr)
 	}
 }
