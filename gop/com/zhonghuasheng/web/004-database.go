eaf08db1cf0dee173c61c9ee8a66780eb87950f4
Luke Chen <xiaoyong6906@126.com>
2022-03-20 14:23:19 +0800

    feat(go): 添加创建表逻辑

--- gop/com/zhonghuasheng/web/004-database.go
+++ gop/com/zhonghuasheng/web/004-database.go
@@ -8,12 +8,22 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "luke:Abcd1234_@tcp(49.235.118.8:3306)/guestdb?charset=utf8")
-	err2 := db.Ping()
+	db, err := sql.Open("mysql", "user:password@tcp(49.235.118.8:3306)/guestdb?charset=utf8")
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	if err2 != nil {
-		fmt.Println(err2)
+
+	// 创建表
+	createSQL := `CREATE TABLE users (
+        id INT AUTO_INCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        created_at DATETIME,
+        PRIMARY KEY (id)
+    );`
+	_, createErr := db.Exec(createSQL)
+	if createErr != nil {
+		fmt.Println(createErr)
 	}
 }
